b939d8d5adf9774e89c58a9b61e9469f206aa9c6
Denis Kotkov <bionic1251@gmail.com>
2024-07-11 00:52:10 +0300

    debug

--- recommender-back/src/apis/forecast.py
+++ recommender-back/src/apis/forecast.py
@@ -86,8 +86,6 @@ class Forecast:
         Updates forecast properties based on the latest parsed data.
         """
         self.valid_times = self.data.data.keys()
-        print('self.valid_times')
-        print(self.valid_times)
         earliest_step = min(self.valid_times)
         self.data_levels = self.data.data[earliest_step].keys()
         self.coordinates = np.dstack(
@@ -155,14 +153,23 @@ class Forecast:
             pois, coordinates)
 
         returned_data = {hour: {} for hour in data}
-        print("returned_data")
-        print(returned_data)
 
+        flag = False
+        flag2 = False
         for hour, hour_data in data.items():
+            if not flag:
+                flag = True
+                print("hour_data")
+                print(str(hour_data)[0:400])
             for poi_coord, nearest in closest_coordinates_fore.items():
                 nearest_str = f"({nearest[0]}, {nearest[1]})"
                 if nearest_str in hour_data:
                     forecast = hour_data[nearest_str]
+                    if not flag2:
+                        flag2 = True
+                        print(nearest_str)
+                        print("forecast")
+                        print(forecast)
                     returned_data[hour][
                         f"{poi_coord[0]}, {poi_coord[1]}"
                     ] = self.parse_forecast(forecast)
