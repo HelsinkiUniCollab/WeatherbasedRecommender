95f7ba9081082149609a513e54ef46ffd91c7b84
Denis Kotkov <bionic1251@gmail.com>
2024-08-02 14:50:10 +0300

    Added output

--- recommender-front/src/App.jsx
+++ recommender-front/src/App.jsx
@@ -168,6 +168,10 @@ function App() {
       }
       const openHour = timeToNumber(p.hours[0]);
       const closeHour = timeToNumber(p.hours[1]);
+      if (!(selectedTime >= openHour && selectedTime <= closeHour)) {
+        console.log(selectedTime);
+        console.log(p);
+      }
       return selectedTime >= openHour && selectedTime <= closeHour;
     });
     setPoiData(filteredData);
